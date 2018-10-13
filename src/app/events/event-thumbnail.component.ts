4d4d69a8430c0110f127aea6bf77b3b258046209
Miguel Sanz <sanzmartinmiguel@gmail.com>
2018-10-13 16:30:02 +0200

    Exploring the Angular template syntax

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,19 +4,32 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'events-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: $\{{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div
+        [ngClass]="{green: event?.time === '8:00 am', bold: event?.time === '8:00 am'}"
+        [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: $\{{event?.price}}</div>
+      <div *ngIf="event?.location">
+        <span>Location: {{event?.location?.address}}</span>
         <span>&nbsp;</span>
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div *ngIf="event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
       </div>
     </div>
   `,
   styles: [`
-    .pad-left {margin-left: 10px; }
+    .green { color: #003300 !important; }
+    .bolc { font-weight: bold; }
+    .thumbnail { min-height: 210px; }
+    .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
   `]
 })
