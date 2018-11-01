7566a3f0d7135e0c62471ef741d63fe62eba98ad
Miguel Sanz <sanzmartinmiguel@gmail.com>
2018-11-01 16:56:33 +0100

    Reusing components with content projection

---
+++ src/app/common/collapsible-well.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'events-collapsible-well',
+  template: `
+    <div (click)="toggleContent()" class="well pointable">
+      <h4>
+        <ng-content select="[well-title]"></ng-content>
+      </h4>
+      <ng-content select="[well-body]" *ngIf="visible"></ng-content>
+    </div>
+  `
+})
+export class CollapsibleWellComponent {
+  visible = true;
+
+  toggleContent() {
+    this.visible = !this.visible;
+  }
+}
